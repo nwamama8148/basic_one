17af6b8c1f07f1846a5454c5862282d058f971d7
nwamama-josh <davidjosiah44@gmail.com>
2024-07-18 23:37:19 +0100

    ide_commit

--- lib/drawer_page.dart
+++ lib/drawer_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class DrawerPage extends StatelessWidget {
-  const DrawerPage({super.key});
+  const DrawerPage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: const [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Drawer Header',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,10 +1,95 @@
+import 'package:basic_one/create_account.dart';
+import 'package:basic_one/my_home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'drawer_page.dart';
+
+Color colGrey = const Color(0xFFFEFFFE);
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.notifications_active),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          ],
+        ),
+        drawer: const DrawerPage(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //HEADER TEXT
+              const Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: const Text(
+                  'Welcome, Jessie.',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'DM Sans',
+                      fontSize: 34),
+                ),
+              ),
+              //HEADER TEXT ENDS HERE
+
+              const SizedBox(
+                height: 15,
+              ),
+
+              // DASHBOARD HERE
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: authsBackColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                height: 125,
+                width: 354,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Your total asset portfolio',
+                      style: TextStyle(
+                        color: authsColorFill,
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          'N203,935',
+                          style: TextStyle(
+                            color: authsColorFill,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32,
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {}, child: const Text('Invest Now'))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              // DASHBOARD ENDS HERE
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
