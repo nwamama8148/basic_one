86e4eea752ebe24680b51fe7459151a280cba31f
nwamama-josh <davidjosiah44@gmail.com>
2024-07-18 21:17:49 +0100

    third commit

--- lib/create_account.dart
+++ lib/create_account.dart
@@ -1,4 +1,9 @@
+import 'package:basic_one/login_page.dart';
+import 'package:basic_one/welcome_page.dart';
 import 'package:flutter/material.dart';
+
+Color authsBackColor = const Color(0xFF31A062);
+Color authsFillColor = const Color(0xFFFFFFFF);
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
@@ -9,6 +14,7 @@ class CreateAccount extends StatefulWidget {
 
 class _CreateAccountState extends State<CreateAccount> {
   final _formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -52,22 +58,130 @@ class _CreateAccountState extends State<CreateAccount> {
                   // HEADER TEXT SECTION ENDS HERE
 
                   // FORM FIELDS HERE
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(18.0),
-                          child: TextFormField(
+                  Padding(
+                    padding: const EdgeInsets.only(top: 98.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Full Name',
                               hintText: 'Enter your full name',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(14.0),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your full name';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email Address',
+                              hintText: 'Enter your email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(14.0),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              hintText: 'Enter a unique Password',
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(14.0),
+                                ),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your full name';
+                              }
+                              return null;
+                            },
+                          ),
+                          // FORM FIELDS ENDS HERE
+
+                          const SizedBox(
+                            height: 25,
+                          ),
+
+                          // SIGNUP BUTTONS HERE
+                          ConstrainedBox(
+                            constraints: const BoxConstraints(
+                                minWidth: 354.00, minHeight: 60.0),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: authsBackColor,
+                                foregroundColor: authsFillColor,
+                              ),
+                              onPressed: () {
+                                if (_formKey.currentState != null) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Processing Data'),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: const Text(
+                                'Create Account',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'SF Pro'),
+                              ),
                             ),
                           ),
-                        )
-                      ],
+                          const SizedBox(
+                            height: 15,
+                          ),
+
+                          // LOGIN BUTTONS HERE
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const WelcomePage(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 19,
+                                color: authsBackColor,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
